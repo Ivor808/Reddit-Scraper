4e4bdb80cb5932f518ff4d8e44ac8ee7324ee2d9
Ivor808 <ivor808@gmail.com>
2019-06-17 20:24:14 -0700

    Updated so program uses mostly functions

--- d.py
+++ d.py
@@ -2,18 +2,16 @@ import praw, requests, csv,obo
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+import time
+start_time = time.time()
 reddit = praw.Reddit(client_id = 'pnJiGPj9YrSycQ',
                      client_secret = 'g8WNac9gNqW3al5nTCPVcIV9SW8',
                      username ='pyTaste',
                      password='kailua',
                      user_agent='wordCloud')
 
-subreddit = reddit.subreddit('politics')
-hot_sub = subreddit.top(limit=40)
 
-
-def wordlisttofreqdict(wordlist):
+def word_list_to_freq_dict(wordlist):
     wordfreq = [wordlist.count(p) for p in wordlist]
     return dict(zip(wordlist, wordfreq))
 
@@ -25,30 +23,46 @@ def sortfreqdict(freqdict):
     return sortme
 
 
-def removestopwords(wordlist, stopwords):
+def remove_stopwords(wordlist, stopwords):
     return [w for w in wordlist if w not in stopwords]
 
 
-hot_words = []
-hot_freq = []
-hot_dict = {}
+def get_top_submissions(subreddit_name, number_of_submissions):
+    sb = reddit.subreddit(subreddit_name)
+    sb_submissions = sb.top(limit=number_of_submissions)
+    top_words = []
+    for submission in sb_submissions:
+        if not submission.stickied:
+            words = submission.title.split()
+            top_words.extend(words)
+    top_words_filtered = remove_stopwords(top_words, obo.stopwords)
+    top_words_frequency_dict = word_list_to_freq_dict(top_words_filtered)
+    return top_words_frequency_dict
 
-for submission in hot_sub:
-    if not submission.stickied:
-        words = submission.title.split()
-        hot_words.extend(words)
-        hot_words = removestopwords(hot_words, obo.stopwords)
-        hot_dict = wordlisttofreqdict(hot_words)
-        keylist = [key for key in hot_dict]
-        vallist = [hot_dict[key] for key in hot_dict]
+
+def create_dataframe_from_dict(dictionary):
+    keylist = [key for key in dictionary]
+    vallist = [dictionary[key] for key in dictionary]
+    df = pd.DataFrame(list(zip(keylist, vallist)), columns=['word', 'Frequency'])
+    df.sort_values(by=['Frequency'], inplace=True, ascending=False)
+    return df
 
 
+def dataframe_to_csv(df):
+    df.to_csv('data.csv')
+    return None
+
+
+top_submission_words_dict = get_top_submissions('leagueoflegends', 500)
+df = create_dataframe_from_dict(top_submission_words_dict)
+print(df)
+print("--- %s seconds ---" % (time.time() - start_time))
 print('done')
 
-df = pd.DataFrame(list(zip(keylist,vallist)), columns= ['word', 'Frequency'])
-df.sort_values(by =['Frequency'], inplace = True, ascending=False)
-#df.to_csv('data.csv')
-plt.plot.bar(rot=0)
+# df = pd.DataFrame(list(zip(keylist,vallist)), columns= ['word', 'Frequency'])
+# df.sort_values(by =['Frequency'], inplace = True, ascending=False)
+# #df.to_csv('data.csv')
+
 
 # TO DO: graph somehow
 # test
