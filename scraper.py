ac315fb71ea50055169394706a4165989bb560e1
Ivor808 <ivor808@gmail.com>
2019-06-18 23:27:21 -0700

    Messed with stemmer
    added todo list

--- scraper.py
+++ scraper.py
@@ -1,11 +1,9 @@
-import praw, requests, csv,obo
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+import praw
+import obo
 import time
 from wordcloud import WordCloud
 import json
-
+import matplotlib
 
 start_time = time.time()
 
@@ -32,40 +30,8 @@ def word_list_to_freq_dict(wordlist):
     return dict(zip(wordlist, wordfreq))
 
 
-def sortfreqdict(freqdict):
-    sortme = [(freqdict[key], key) for key in freqdict]
-    sortme.sort()
-    sortme.reverse()
-    return sortme
-
-
 def remove_stopwords(wordlist, stopwords):
     return [w for w in wordlist if w not in stopwords]
-
-
-def get_top_submissions(subreddit_name, number_of_submissions):
-    sb_submissions = __get_subreddit_object(subreddit_name).top(limit=number_of_submissions)
-    top_words = []
-    for submission in sb_submissions:
-        if not submission.stickied:
-            words = submission.title.split()
-            top_words.extend(words)
-    top_words_filtered = remove_stopwords(top_words, obo.stopwords)
-    top_words_frequency_dict = word_list_to_freq_dict(top_words_filtered)
-    return top_words_frequency_dict
-
-
-def create_dataframe_from_dict(dictionary):
-    keylist = [key for key in dictionary]
-    vallist = [dictionary[key] for key in dictionary]
-    df = pd.DataFrame(list(zip(keylist, vallist)), columns=['word', 'Frequency'])
-    df.sort_values(by=['Frequency'], inplace=True, ascending=False)
-    return df
-
-
-def dataframe_to_csv(df):
-    df.to_csv('data.csv')
-    return None
 
 
 def top_submission_to_word_cloud(subreddit_name, number_of_submissions):
@@ -80,17 +46,13 @@ def top_submission_to_word_cloud(subreddit_name, number_of_submissions):
     wc.to_file('test.png')
 
 
-top_submission_to_word_cloud('g', 200)
-
+top_submission_to_word_cloud('politics', 500)
 
 print("--- %s seconds ---" % (time.time() - start_time))
 print('done')
 
-# df = pd.DataFrame(list(zip(keylist,vallist)), columns= ['word', 'Frequency'])
-# df.sort_values(by =['Frequency'], inplace = True, ascending=False)
-# #df.to_csv('data.csv')
-
-
-# TO DO:
-# - Graph
-# - Exception handling for non existent subreddit
+# TODO: Maybe create the wordcloud using a stemmer algorithm
+# TODO: Package the script
+# TODO: Add handling of invalid subreddits
+# TODO: Create wordcloud generators for HOT and maybe other types
+# TODO: Create a main where user can input subreddit sort type and sort
