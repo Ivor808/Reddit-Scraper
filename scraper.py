f29fee86ef89bfe41b56d69c796d94ec68ca16ce
Ivor808 <ivor808@gmail.com>
2019-06-29 14:08:14 -0700

    Added comments

--- scraper.py
+++ scraper.py
@@ -4,10 +4,13 @@ from wordcloud import WordCloud
 import json
 import matplotlib
 
-start_time = time.time()
 
-
-def load_json_file(file):
+def __load_json_file(file):
+    """
+    Loads the JSON config file where the user inputs their reddit API info
+    :param file: config file with user info
+    :return: list of user info
+    """
     with open(file, 'r') as json_file:
         config_file = json.load(json_file)
     return config_file
@@ -15,11 +18,11 @@ def load_json_file(file):
 
 def __get_subreddit_object(subreddit_name):
     """
-
-    :param subreddit_name:
+    Creates a subreddit object utilizing the PRAW library
+    :param subreddit_name: The desired subreddit
     :return:
     """
-    config = load_json_file('config.json')
+    config = __load_json_file('config.json')
     reddit = praw.Reddit(client_id=config['clientId'],
                          client_secret=config['clientSecret'],
                          username=config['username'],
@@ -29,12 +32,14 @@ def __get_subreddit_object(subreddit_name):
     return sb
 
 
-def word_list_to_freq_dict(wordlist):
-    wordfreq = [wordlist.count(p) for p in wordlist]
-    return dict(zip(wordlist, wordfreq))
-
-
 def top_submission_to_word_cloud(subreddit_name, number_of_submissions):
+    """
+    Creates a wordcloud of the top posts from a subreddit and saves it as an image
+
+    :param subreddit_name: The subreddit to be queried
+    :param number_of_submissions: How many posts to use in generating the word cloud
+    :return: No return
+    """
     sb_submissions = __get_subreddit_object(subreddit_name).top(limit=number_of_submissions)
     top_submission = []
     for submission in sb_submissions:
@@ -48,13 +53,10 @@ def top_submission_to_word_cloud(subreddit_name, number_of_submissions):
 
 def main():
     number_of_posts_num = 50
+    start_time = time.time()
     print("Subreddit to WordCloud Generator")
     while True:
         subreddit_name = input("What subreddit would you like a WordCloud of? (Do not include /r/): ")
-        try:
-            sb_submissions = __get_subreddit_object(subreddit_name).top(limit=1)
-        except:
-            print("Not a valid subreddit")
 
         number_of_posts = input("How many top posts do you want to use? Max of 1000: ")
         try:
@@ -65,15 +67,14 @@ def main():
         if number_of_posts_num <= 0:
             print('Number is less than zero')
         else:
-            print(type(number_of_posts_num))
-            print(type(subreddit_name))
-            top_submission_to_word_cloud(subreddit_name, number_of_posts_num)
-            break
+
+            try:
+                top_submission_to_word_cloud(subreddit_name, number_of_posts_num)
+            except:
+                print('Not valid subreddit ')
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
 
 main()
-# TODO: Maybe create the wordcloud using a stemmer algorithm
-# TODO: Package the script
-# TODO: Add handling of invalid subreddits
-# TODO: Create wordcloud generators for HOT and maybe other types
-# TODO: Create a main where user can input subreddit sort type and sort
-# TODO: Add comments for each function
+
